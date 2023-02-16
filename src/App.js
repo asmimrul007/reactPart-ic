a00099cee5fea91bba4832fd821a61dc0f84c044
asmimrul <asmimrul@gmail.com>
2023-02-17 00:18:07 +0600

    somechanges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
-function App() {
+const App = () => {
+
+  const [counter, setCounter]= useState(0)
+
+  
+  //increase handler
+  const increaseHandler = (value) =>{
+    setCounter(counter + value) 
+  }
+
+  const decreaseHandler = (value) =>{
+    setCounter(counter - value)
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h2>
+        The Value of the counter is {counter}
+      </h2>
+      <button onClick={() =>increaseHandler(1)}>Increased By 1 </button>
+      <button onClick={() =>decreaseHandler(1)}>Decreased By 1</button><hr/>
+      <button onClick={() =>increaseHandler(100)}>Increased By 100</button>
+      <button onClick={() =>decreaseHandler(100)}>Decreased By 100</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
